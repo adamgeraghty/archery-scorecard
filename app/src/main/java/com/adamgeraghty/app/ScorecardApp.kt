21fc95c5813521859ac6585b8010f388f8680aef
Adam Geraghty <adam.geraghty@marinosoftware.com>
2024-05-15 17:09:37 +0100

    Add Timber debug logs

--- app/src/main/java/com/adamgeraghty/app/ScorecardApp.kt
+++ app/src/main/java/com/adamgeraghty/app/ScorecardApp.kt
@@ -2,6 +2,12 @@ package com.adamgeraghty.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class ScorecardApp : Application()
+class ScorecardApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
